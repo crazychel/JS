934b556b3ab79236e493c33d52d33e62ead00e4f
crazychel <48129985+crazychel@users.noreply.github.com>
2019-03-01 21:20:49 +0200

    Add files via upload

---
+++ 1/skript1.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let money, time, appData, expenses, optionalExpenses, income, savings;
+money = prompt('Ваш бюджет на месяц?');
+time = prompt('Введите дату в формате YYYY-MM-DD');
+
+appData = {
+    money,
+    time
+};
+
+income = {};
+savings = false;
+
+
+console.log(appData)
+
+let button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    let appData1 = {
+            expenses: {}
+        },
+        answerA = document.querySelectorAll('input')[0].value,
+        answerB = document.querySelectorAll('input')[1].value;
+    appData1.expenses[answerA] = answerA;
+    appData1.expenses.answerB = answerB;
+    console.log(appData1);
+    alert('Бюджет на 1 день = ' + money / 30)
+});
