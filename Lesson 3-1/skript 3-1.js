cda3d647997b9fe8b8e6094679da69e1fa6a83ef
crazychel <48129985+crazychel@users.noreply.github.com>
2019-03-05 13:29:55 +0200

    Add files via upload

--- Lesson 3-1/skript 3-1.js
+++ Lesson 3-1/skript 3-1.js
@@ -40,65 +40,7 @@ chooseExpenses();
 
 
 appData.moneyPerDay = (appData.budget / 30).toFixed(1);
-
 alert('Ежедневный бюджет: ' + appData.moneyPerDay);
-
-// let way = +prompt('Какой способ для цыкла использовать? (1, 2, 3)', '') // будет 3 варианта, 3 цикла в одном js коде
-// let x = appData.moneyPerDay // заменяю только потому что так глазу приятнее
-
-// switch (way) {
-
-
-//     case 1: //этот цикл из видео-обучения
-//         if (appData.moneyPerDay < 100) {
-//             console.log('Минимальный уровень достатка');
-//         } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-//             console.log('Средний уровень достатка');
-//         } else if (appData.moneyPerDay >= 2000) {
-//             console.log('Высокий уровень достатка');
-//         } else {
-//             console.log('Произошла ошибка');
-//         };
-//         break;
-
-
-//     case 2: // Второй вид цикла      
-//         switch (x) {
-//             case (x < 100 ? x : null):
-//                 console.log('Минимальный уровень достатка');
-//                 break;
-//             case (x > 100 && x < 2000 ? x : null):
-//                 console.log('Средний уровень достатка');
-//                 break;
-//             case (x >= 2000 ? x : null):
-//                 console.log('Высокий уровень достатка');
-//                 break;
-//             default:
-//                 console.log('Что-то пошло не так во втором switch');
-//         };
-//         break;
-
-
-//     case 3: // третий вид 
-//         while (x < 100) {
-//             console.log('Минимальный уровень достатка');
-//             break;
-//         };
-//         while (x > 100 && x < 2000) {
-//             console.log('Средний уровень достатка');
-//             break;
-//         };
-//         while (x >= 2000) {
-//             console.log('Высокий уровень достатка');
-//             break;
-//         };
-//         break;
-
-
-//     default: // вот и все) Работает!
-//         console.log('Ошибка связана с way');
-
-// };
 
 function checkSavings() {
     if (appData.savings == true) {
@@ -129,13 +71,12 @@ detectLevel()
 alert('Расчет дневного бюджета: ' + appData.detectDayBudget);
 
 function chooseOptExpenses() {
-    let i = 1,
-        n;
+    let i = 1; //optionalExpenses: { 1 : “ответ на вопрос”}
     while (i < 4) {
-        n = +prompt('Статья необязательных расходов?');
-        appData.optionalExpenses[i] = n;
+        let OptExpenses = +prompt('Статья необязательных расходов?');
+        appData.optionalExpenses[i] = OptExpenses;
         i++
-        n++
+        OptExpenses++
     }
 };
 chooseOptExpenses()
